85cbf0c553547665e9b271a27c55ee33ac87e5e1
Daniel Contreras <josedanielcontrerasperez510@gmail.com>
2025-03-14 15:10:47 -0700

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css'
+import { mdiHome } from "@mdi/js"
+import Icon from "@mdi/react"
+
 
 function App() {
 
   return (
     <>
       <h1>Hola mundo</h1>
+      <Icon path={mdiHome} size={1} />
     </>
   )
 }
