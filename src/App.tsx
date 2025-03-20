80e32af626478db6eba5e7ca47561caa8df49691
Daniel Contreras <josedanielcontrerasperez510@gmail.com>
2025-03-19 18:42:06 -0700

    Codition component is valid

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { useMemo } from "react";
 import BubgetForm from "./components/BubgetForm";
 import ThemeProvider from "./components/theme/ThemeProvider";
+import { useBudget } from "./hooks/useBudget";
+import BudgetTracker from "./components/BudgetTracker";
 
 export default function App() {  
+  const {state } = useBudget();
+  console.log(state.budget > 0);
+
+  const isValidBudget = useMemo(() => state.budget > 0 ,[state.budget])
+  
   return (
     <div className="min-h-screen flex flex-col bg-white dark:bg-gray-900 text-black dark:text-white">
       {/* Contenedor del encabezado */}
@@ -13,7 +21,7 @@ export default function App() {
       {/* Contenido principal */}
       <main className="flex-grow flex flex-col items-center justify-center text-center p-6">
         <div className="max-w-3xl mx-auto w-full bg-white shadow-lg rounded-lg mt-10 p-10 dark:bg-gray-800">
-          <BubgetForm />
+          {isValidBudget ? <BudgetTracker /> : <BubgetForm />}
         </div>
       </main>
 
---
+++ src/components/BudgetTracker.tsx
@@ -0,0 +1,8 @@
+
+export default function BudgetTracker() {
+  return (
+    <>
+      <h1>Hola desde BudgetTracket</h1>
+    </>
+  )
+}
