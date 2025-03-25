c31468965b4ecc989bbfbba9f519778dcae53592
Daniel Contreras <josedanielcontrerasperez510@gmail.com>
2025-03-24 17:24:24 -0700

    Writting in expense state

--- src/components/dialogs/ExpenseModal.tsx
+++ src/components/dialogs/ExpenseModal.tsx
@@ -3,49 +3,36 @@ import Icon from '@mdi/react';
 import { useBudget } from '../../hooks/useBudget';
 import ExpenseForm from '../form/ExpenseForm';
 
-
-
 export default function ExpenseModal() {
     const { state, dispatch } = useBudget();
     return (
-        <div className='fixed right-5 bottom-5 flex items-center justify-center'>
-            <button type='button' onClick={() => dispatch({ type: 'show-modal' })} className='rounded-full bg-mango-yellow w-16 h-16'>
-                <Icon path={mdiPlus} size={2} className='m-auto' />
+        <div className="fixed right-5 bottom-5 flex items-center justify-center">
+            <button
+                type="button"
+                onClick={() => dispatch({ type: 'show-modal' })}
+                className="rounded-full bg-mango-yellow w-16 h-16"
+            >
+                <Icon path={mdiPlus} size={2} className="m-auto" />
             </button>
 
-            {
-                state.modal && (
-                    <div
-                        className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-500 ease-out ${state.modal ? 'opacity-100' : 'opacity-0 pointer-events-none'
-                            }`}
-
-                    >
-                        <div
-                            className={`bg-white p-6 dark:bg-gray-900 rounded-lg shadow-xl w-full transform transition-all duration-500 ease-out 
-                                ${state.modal ? 'scale-100 opacity-100' : 'scale-50 opacity-0'}
-                                sm:max-w-sm md:max-w-md lg:max-w-3xl xl:max-w-4xl`}
-                        >
-                            <h2 className="text-xl font-medium text mb-10 text-left"><span className='bg-custom-blue px-4 py-2 rounded-lg text-white'>Nuevo gasto</span></h2>
-                            <ExpenseForm />
-                            {/* <div className="mt-10 flex flex-col lg:flex-row justify-between gap-3 w-full">
-                                <button
-                                    onClick={() => dispatch({ type: 'close-modal' })}
-                                    className="block w-full py-2 uppercase text-white rounded-lg bg-red-500 hover:bg-red-900"
-                                >
-                                    Cerrar
-                                </button>
-                                <button
-                                    onClick={() => dispatch({ type: 'close-modal' })}
-                                    className="block w-full px-4 py-2 uppercase bg-custom-blue hover:bg-cyan-800 text-white rounded-lg"
-                                >
-                                    Agregar
-                                </button>
-                            </div> */}
-
-                        </div>
-                    </div>
-                )
-            }
+            {/* Modal siempre presente pero con clases de animación */}
+            <div
+                className={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 transition-opacity duration-500 ease-out 
+                ${state.modal ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
+            >
+                <div
+                    className={`bg-white p-6 dark:bg-gray-900 rounded-lg shadow-xl w-full transform transition-all duration-500 ease-out 
+                    ${state.modal ? 'scale-100 opacity-100' : 'scale-75 opacity-0'} 
+                    sm:max-w-sm md:max-w-md lg:max-w-3xl xl:max-w-4xl`}
+                >
+                    <h2 className="text-xl font-medium mb-10 text-left">
+                        <span className="bg-custom-blue px-4 py-2 rounded-lg text-white">
+                            Nuevo gasto
+                        </span>
+                    </h2>
+                    <ExpenseForm />
+                </div>
+            </div>
         </div>
-    )
+    );
 }
--- src/components/form/ExpenseForm.tsx
+++ src/components/form/ExpenseForm.tsx
@@ -1,81 +1,114 @@
-import { FormEvent } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { categories } from "../../data/categories";
 import { useBudget } from "../../hooks/useBudget";
-
-type ValuePiece = Date | null;
-type Value = ValuePiece | [ValuePiece, ValuePiece];
+import { DraftExpense } from "../../types";
+import moment from "moment";
 
 export default function ExpenseForm() {
     const { dispatch } = useBudget();
+    const [expense, setExpense] = useState<DraftExpense>({
+        amount: 0,
+        expenseName: '',
+        category: '',
+        date: moment().toDate(),
+    });
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault(); // Evita que el formulario se recargue
-        dispatch({ type: 'close-modal' }); // Aquí podrías manejar la lógica de envío
+        e.preventDefault();
+        dispatch({ type: 'close-modal' });
     };
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        const { name, value } = event.target;
+        const isAmountField = ['amount'].includes(name);
+        setExpense({
+            ...expense,
+            [name]: isAmountField ? +value : value
+        });
+    };
+
+    const handleChangeDate = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setExpense(prevExpense => ({
+            ...prevExpense,
+            date: moment(event.target.value, 'YYYY-MM-DD').toDate(),
+        }));
+    };
+
 
     return (
         <form className="space-y-5" onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-5">
-                <div className="relative">
-                    <input
-                        className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                    />
-                    <label className="absolute cursor-text bg-white dark:bg-gray-900 px-1 left-2.5 top-4 text-slate-400 text-sm transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
-                        Nombre del gasto
-                    </label>
-                </div>
+            {/* Nombre del gasto */}
+            <div className="relative">
+                <input
+                    type="text"
+                    name="expenseName"
+                    value={expense.expenseName}
+                    onChange={handleChange}
+                    className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                />
+                <label className="absolute cursor-auto bg-white dark:bg-gray-900 px-1 left-2.5 -top-2 text-xs text-slate-400 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
+                    Nombre del gasto
+                </label>
             </div>
-            <div className="flex flex-col gap-2">
-                <div className="relative">
-                    <input
-                        type="number"
-                        className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                    />
-                    <label className="absolute cursor-text bg-white dark:bg-gray-900 px-1 left-2.5 top-4 text-slate-400 text-sm transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
-                        Cantidad
-                    </label>
-                </div>
+
+            {/* Cantidad */}
+            <div className="relative">
+                <input
+                    type="number"
+                    name="amount"
+                    value={expense.amount}
+                    onChange={handleChange}
+                    className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                />
+                <label className="absolute cursor-auto bg-white dark:bg-gray-900 px-1 left-2.5 -top-2 text-xs text-slate-400 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
+                    Cantidad
+                </label>
             </div>
-            <div className="flex flex-col gap-2">
-                <div className="relative">
-                    <select
-                        className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                    >
-                        <option value="">Seleccione la categoría</option>
-                        {
-                            categories.map(category => (
-                                <option value={category.id} key={category.id}>
-                                    {category.name}
-                                </option>
-                            ))
-                        }
-                    </select>
-                    <label className="absolute cursor-auto bg-white dark:bg-gray-900 px-1 left-2.5 -top-2 text-xs text-slate-400 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
-                        Categoría
-                    </label>
-                </div>
+
+            {/* Categoría */}
+            <div className="relative">
+                <select
+                    name="category"
+                    value={expense.category}
+                    onChange={handleChange}
+                    className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                >
+                    <option value="">Seleccione la categoría</option>
+                    {categories.map(category => (
+                        <option value={category.id} key={category.id}>
+                            {category.name}
+                        </option>
+                    ))}
+                </select>
+                <label className="absolute cursor-auto bg-white dark:bg-gray-900 px-1 left-2.5 -top-2 text-xs text-slate-400 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
+                    Categoría
+                </label>
             </div>
-            <div className="flex flex-col gap-2">
-                <div className="relative">
-                    <input
-                        type="date"
-                        className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
-                    />
-                    <label className="absolute cursor-auto bg-white dark:bg-gray-900 px-1 left-2.5 -top-2 text-xs text-slate-400 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
-                        Fecha
-                    </label>
-                </div>
+
+            {/* Fecha */}
+            <div className="relative">
+                <input
+                    type="date"
+                    value={moment(expense.date).format('YYYY-MM-DD')}
+                    onChange={handleChangeDate}
+                    className="peer w-full dark:bg-gray-900 placeholder:text-slate-400 text-sm border border-slate-200 rounded-lg px-4 py-4 transition duration-300 ease focus:outline-none focus:border-slate-400 hover:border-slate-300 shadow-sm focus:shadow"
+                />
+                <label className="absolute cursor-auto bg-white dark:bg-gray-900 px-1 left-2.5 -top-2 text-xs text-slate-400 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:text-slate-400 peer-focus:scale-90">
+                    Fecha
+                </label>
             </div>
+
+            {/* Botones */}
             <div className="flex flex-col lg:flex-row justify-between gap-3 w-full">
                 <button
-                    type="button" // Cambiado de "submit" a "button"
+                    type="button"
                     onClick={() => dispatch({ type: 'close-modal' })}
                     className="block w-full py-2 uppercase dark:text-white rounded-lg dark:bg-red-500 text-red-700 border-2"
                 >
                     Cerrar
                 </button>
                 <button
-                    type="submit" // Solo este botón envía el formulario
+                    type="submit"
                     className="block w-full px-4 py-2 uppercase bg-custom-blue hover:bg-cyan-800 text-white rounded-lg"
                 >
                     Agregar
